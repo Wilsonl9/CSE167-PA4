7a93914d0da6c8eac926f4d2cacfabc4940de4e5
Wilson <Wilsonlam9999@gmail.com>
2018-03-20 16:14:37 -0700

    fixed the TerrainGenerator to generate better looking terrain

--- TerrainGenerator.cpp
+++ TerrainGenerator.cpp
@@ -3,6 +3,7 @@
 TerrainGenerator::TerrainGenerator()
 {
 	srand(time(NULL));
+	mat2 = NULL;
 }
 
 TerrainGenerator::~TerrainGenerator()
@@ -27,7 +28,9 @@ Geometry * TerrainGenerator::createTerrain(int N)
 	mat.clear();
 	Geometry* terrain = new Geometry();
 	float i;
-	float levels = std::max((int)(log(N) / log(2)), N / 2);
+	//float levels = std::max((int)(log(N) / log(2)), N / 2);
+	//float levels = (int)(log(N) / log(2)) * N;
+	float levels = (int)(log2(N) + 10) * N;
 	float row;
 	float col;
 	bool alternateToRow;
@@ -37,6 +40,7 @@ Geometry * TerrainGenerator::createTerrain(int N)
 	mat[N - 1][N - 1] = RANDOMIZE(MINHEIGHT, MAXHEIGHT);
 	float blockSize;
 	float halfBlockSize;
+	float decrease = 1;
 	for (i = 1; i < levels; i *= 2)
 	{
 		blockSize = (((float)N) - 1.0f) / i;
@@ -45,7 +49,11 @@ Geometry * TerrainGenerator::createTerrain(int N)
 		{
 			for (col = 0; col <= ((float)(N - 1)) - halfBlockSize; col += blockSize)
 			{
-				mat[row + halfBlockSize][col + halfBlockSize] = MEAN4(mat[row][col], mat[row + blockSize][col], mat[row][col + blockSize], mat[row + blockSize][col + blockSize]) + PLUSMINUS(15);
+				mat[row + halfBlockSize][col + halfBlockSize] = MEAN4(mat[row][col], mat[row + blockSize][col], mat[row][col + blockSize], mat[row + blockSize][col + blockSize])
+					//+ PLUSMINUS(VARIANCE * ((0.5f / i) + .5f));
+					+ PLUSMINUS(VARIANCE * decrease);
+				/*std::cout << "1: " << mat[row][col] << " 2: " << mat[row + blockSize][col] << " 3: " << mat[row][col + blockSize] << " 4: " << mat[row + blockSize][col + blockSize] << " mean: " << MEAN4(mat[row][col], mat[row + blockSize][col], mat[row][col + blockSize], mat[row + blockSize][col + blockSize]) << std::endl;
+				std::cin.ignore();*/
 			}
 		}
 		alternateToRow = true;
@@ -56,11 +64,12 @@ Geometry * TerrainGenerator::createTerrain(int N)
 				for (col = halfBlockSize; col < ((float)N - 1); col += blockSize)
 				{
 					mat[row][col] = (row 
-							? ((row < ((float)N - 1))
-								? MEAN4(mat[row - halfBlockSize][col], mat[row + halfBlockSize][col], mat[row][col + halfBlockSize], mat[row][col - halfBlockSize])
-								: MEAN3(mat[row - halfBlockSize][col], mat[row][col + halfBlockSize], mat[row][col - halfBlockSize]))
-							: MEAN3(mat[row + halfBlockSize][col], mat[row][col + halfBlockSize], mat[row][col - halfBlockSize]))
-						+ PLUSMINUS(VARIANCE);
+						? ((row < ((float)N - 1))
+							? MEAN4(mat[row - halfBlockSize][col], mat[row + halfBlockSize][col], mat[row][col + halfBlockSize], mat[row][col - halfBlockSize])
+							: MEAN3(mat[row - halfBlockSize][col], mat[row][col + halfBlockSize], mat[row][col - halfBlockSize]))
+						: MEAN3(mat[row + halfBlockSize][col], mat[row][col + halfBlockSize], mat[row][col - halfBlockSize]))
+						//+ PLUSMINUS(VARIANCE * ((0.5f / i) + .5f));
+						+PLUSMINUS(VARIANCE * decrease);
 				}
 				alternateToRow = !alternateToRow;
 			}
@@ -73,7 +82,78 @@ Geometry * TerrainGenerator::createTerrain(int N)
 							? MEAN4(mat[row - halfBlockSize][col], mat[row + halfBlockSize][col], mat[row][col + halfBlockSize], mat[row][col - halfBlockSize])
 							: MEAN3(mat[row - halfBlockSize][col], mat[row + halfBlockSize][col], mat[row][col - halfBlockSize]))
 						: MEAN3(mat[row - halfBlockSize][col], mat[row + halfBlockSize][col], mat[row][col + halfBlockSize]))
-						+ PLUSMINUS(VARIANCE);
+						//+ PLUSMINUS(VARIANCE * ((0.5f / i) + .5f));
+						+PLUSMINUS(VARIANCE * decrease);
+				}
+				alternateToRow = !alternateToRow;
+			}
+		}
+		decrease = decrease * RETAIN;
+	}
+	calculateVerticesAndIndices(terrain->getVertices(), terrain->getNormals(), terrain->getIndices(), N, levels);
+	terrain->init();
+	return terrain;
+}
+
+Geometry * TerrainGenerator::createTerrain2(int N, int divisions)
+{
+	delete mat2;
+	mat2 = new float[N * divisions * N * divisions];
+	Geometry* terrain = new Geometry();
+	float i;
+	float levels = divisions * N;
+	float row;
+	float col;
+	bool alternateToRow;
+	mat2[AT(0, 0, divisions * N)] = RANDOMIZE(MINHEIGHT, MAXHEIGHT);
+	mat2[AT(0, N - 1, divisions * N)] = RANDOMIZE(MINHEIGHT, MAXHEIGHT);
+	mat2[AT(N - 1, 0, divisions * N)] = RANDOMIZE(MINHEIGHT, MAXHEIGHT);
+	mat2[AT(N - 1, N - 1, divisions * N)] = RANDOMIZE(MINHEIGHT, MAXHEIGHT);
+	float blockSize;
+	float halfBlockSize;
+	for (i = 1; i < levels; i *= 2)
+	{
+		blockSize = (((float)N) - 1.0f) / i;
+		halfBlockSize = blockSize / 2;
+		for (row = 0; row <= ((float)(N - 1)) - halfBlockSize; row += blockSize)
+		{
+			for (col = 0; col <= ((float)(N - 1)) - halfBlockSize; col += blockSize)
+			{
+				mat2[AT(row + halfBlockSize, col + halfBlockSize, divisions * N)] = MEAN4(mat2[AT(row, col, divisions * N)], mat2[AT(row + blockSize, col, divisions * N)], mat2[AT(row, col + blockSize, divisions * N)], mat2[AT(row + blockSize, col + blockSize, divisions * N)])
+					//+ PLUSMINUS(VARIANCE * levels * (1.0f / (2.0f * std::sqrt(i))));
+					+ PLUSMINUS(VARIANCE / ((float)i));
+				/*std::cout << "1: " << mat[row][col] << " 2: " << mat[row + blockSize][col] << " 3: " << mat[row][col + blockSize] << " 4: " << mat[row + blockSize][col + blockSize] << " mean: " << MEAN4(mat[row][col], mat[row + blockSize][col], mat[row][col + blockSize], mat[row + blockSize][col + blockSize]) << std::endl;
+				std::cin.ignore();*/
+			}
+		}
+		alternateToRow = true;
+		for (row = 0; row <= ((float)N - 1); row += halfBlockSize)
+		{
+			if (alternateToRow)
+			{
+				for (col = halfBlockSize; col < ((float)N - 1); col += blockSize)
+				{
+					mat2[AT(row, col, divisions * N)] = (row
+						? ((row < ((float)N - 1))
+							? MEAN4(mat2[AT(row - halfBlockSize, col, divisions * N)], mat[row + halfBlockSize][col], mat[row][col + halfBlockSize], mat[row][col - halfBlockSize])
+							: MEAN3(mat[row - halfBlockSize][col], mat[row][col + halfBlockSize], mat[row][col - halfBlockSize]))
+						: MEAN3(mat[row + halfBlockSize][col], mat[row][col + halfBlockSize], mat[row][col - halfBlockSize]))
+						//+ PLUSMINUS(VARIANCE * levels * (1.0f / (2.0f * std::sqrt(i))));
+						+ PLUSMINUS(VARIANCE / ((float)i));
+				}
+				alternateToRow = !alternateToRow;
+			}
+			else
+			{
+				for (col = 0; col <= ((float)(N - 1)); col += blockSize)
+				{
+					mat[row][col] = (col
+						? ((col < ((float)(N - 1)))
+							? MEAN4(mat[row - halfBlockSize][col], mat[row + halfBlockSize][col], mat[row][col + halfBlockSize], mat[row][col - halfBlockSize])
+							: MEAN3(mat[row - halfBlockSize][col], mat[row + halfBlockSize][col], mat[row][col - halfBlockSize]))
+						: MEAN3(mat[row - halfBlockSize][col], mat[row + halfBlockSize][col], mat[row][col + halfBlockSize]))
+						+ PLUSMINUS(VARIANCE * levels * (1.0f / (2.0f * std::sqrt(i))));
+					+PLUSMINUS(VARIANCE / ((float)i));
 				}
 				alternateToRow = !alternateToRow;
 			}
@@ -86,13 +166,7 @@ Geometry * TerrainGenerator::createTerrain(int N)
 
 /*void TerrainGenerator::diamondSquare(int N, int row, int col, int blockSize)
 {
-	int halfBlockSize = blockSize / 2;
-	mat[row + halfBlockSize][col + halfBlockSize] = MEAN4(mat[row][col], mat[row + blockSize][col], mat[row][col + blockSize], mat[row + blockSize][col + blockSize]) + PLUSMINUS(15);
-	mat[row + halfBlockSize][col + halfBlockSize] = (row ? MEAN4(mat[row - ][col], mat[row + blockSize][col], mat[row][col + blockSize], mat[row + blockSize][col + blockSize]) : MEAN3(mat[row][col], mat[row + blockSize][col], mat[row][col + blockSize], mat[row + blockSize][col + blockSize])) + PLUSMINUS(15);
-	diamondSquare(N, row, col, halfBlockSize + 1);
-	diamondSquare(N, row + halfBlockSize, col, halfBlockSize + 1);
-	diamondSquare(N, row, col + halfBlockSize, halfBlockSize + 1);
-	diamondSquare(N, row + halfBlockSize, col + halfBlockSize, halfBlockSize + 1);
+	
 }*/
 
 void TerrainGenerator::calculateVerticesAndIndices(std::vector<glm::vec3>& vertices, std::vector<glm::vec3>& normals, std::vector<unsigned int>& indices, int N, int levels)
--- TerrainGenerator.h
+++ TerrainGenerator.h
@@ -4,16 +4,19 @@
 #include<string>
 #include<time.h>
 #include<unordered_map>
+#include<cmath>
 #include"Transform.h"
 #include"Geometry.h"
 
 #define MAXHEIGHT 10
 #define MINHEIGHT 0
-#define VARIANCE 0
+#define VARIANCE 15.0f
+#define RETAIN 0.48f
 #define RANDOMIZE(min,max) (((float)(rand() % ((max) - (min) + 1))) + ((float)(min)))
 #define PLUSMINUS(x) (((2.0f * ((float)(x))) * (((float)((rand() % 100) + 1.0f)) / 100.0f)) - ((float)(x)))
 #define MEAN4(x1,x2,x3,x4) (((float)((x1) + (x2) + (x3) + (x4))) / 4.0f)
 #define MEAN3(x1,x2,x3) (((float)((x1) + (x2) + (x3))) / 3.0f)
+#define AT(x,y,size) ((int)((((float)x) * ((float)size)) + (float)(y)))
 
 class TerrainGenerator
 {
@@ -24,7 +27,9 @@ public:
 
 private:
 	std::unordered_map<float, std::unordered_map<float, float>> mat;
+	float* mat2;
 	Geometry* createTerrain(int N);
+	Geometry* createTerrain2(int N, int divisions = 2);
 	//void diamondSquare(int N, int row, int col, int blockSize);
 	void calculateVerticesAndIndices(std::vector<glm::vec3>& vertices, std::vector<glm::vec3>& normals, std::vector<unsigned int>& indices, int N, int levels);
 };
